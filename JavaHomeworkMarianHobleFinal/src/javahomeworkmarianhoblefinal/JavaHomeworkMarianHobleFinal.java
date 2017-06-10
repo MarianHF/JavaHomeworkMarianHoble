058dca44778e6d692bf372ea6bfdb83f570c3cf8
Marian <Marian@Marian-PC>
2017-06-10 14:03:58 +0300

    Initial commit

---
+++ JavaHomeworkMarianHobleFinal/src/javahomeworkmarianhoblefinal/JavaHomeworkMarianHobleFinal.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javahomeworkmarianhoblefinal;
+
+/**
+ *
+ * @author Marian
+ */
+public class JavaHomeworkMarianHobleFinal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
